91da7194cffb9c93fd28fefb78d8b581b8bcc6bf
Hanokh Aloni <hanokh.aloni@gmail.com>
2025-01-24 23:32:18 +0200

    shaders are way too fast and not really fit

--- main.js
+++ main.js
@@ -75,77 +75,40 @@ void main() {
 `;
 
 const fragmentShader2 = `
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-// Love u Hanna E
+precision highp float;   /// iOS needs high?
 
 uniform float time;
-uniform vec2 mouse;
 uniform vec2 resolution;
 
-float snoise(vec3 uv, float res) {
-    const vec3 s = vec3(1e0, 1e2, 1e3);
+varying vec2 fragCoord;
 
-    uv *= res;
+// "[SH17A] Fireworks" by Martijn Steinrucken aka BigWings/Countfrolic - 2017
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// Based on https://www.shadertoy.com/view/lscGRl
 
-    vec3 uv0 = floor(mod(uv, res)) * s;
-    vec3 uv1 = floor(mod(uv + vec3(1.0), res)) * s;
+#define N(h) fract(sin(vec4(6,9,1,0)*h) * 9e2)
 
-    vec3 f = smoothstep(0.0, 1.0, fract(uv));
+void main(void)
+{
+  vec4 o;
+  vec2 u = fragCoord.xy/resolution.y;
 
-    vec4 v = vec4(uv0.x + uv0.y + uv0.z,
-              uv1.x + uv0.y + uv0.z,
-              uv0.x + uv1.y + uv0.z,
-              uv1.x + uv1.y + uv0.z);
+// proper pixelate
+float s = 500.;
+    u = floor(u * s) / s;
 
-    vec4 r = fract(sin(v * 1e-1) * 1e3);
-    float r0 = mix(mix(r.x, r.y, f.x), mix(r.z, r.w, f.x), f.y);
+  float e, d, i=0.;
+  vec4 p;
 
-    r = fract(sin((v + uv1.z - uv0.z) * 1e-1) * 1e3);
-    float r1 = mix(mix(r.x, r.y, f.x), mix(r.z, r.w, f.x), f.y);
+  for(float i=1.; i<30.; i++) {
+    d = floor(e = i*9.1+time);
+    p = N(d)+.3;
+    e -= d;
+    for(float d=0.; d<5.;d++)
+      o += p*(2.9-e)/1e3/length(u-(p-e*(N(d*i)-.5)).xy);
+  }
 
-    return mix(r0, r1, f.z) * 2.0 - 1.0;
-}
-
-void main() {
-    vec2 p = -0.5 + gl_FragCoord.xy / resolution.xy;
-    p.x *= resolution.x / resolution.y;
-    float lp = .02/length(p);
-    float ap = atan(p.x, p.y);
-
-    float time = time*.04-pow(time, .8)*(1. + .1*cos(time*0.04))*2.;
-
-    float r1 = 0.2;
-    if(lp <= r1){
-        ap -= time*0.1+lp*9.;
-        lp = sqrt(1.-lp/r1)*0.5;
-    }else{
-        ap += time*0.1+lp*2.;
-        lp -= r1;
-    }
-
-    lp = pow(lp*lp, 1./3.);
-
-    p = lp*vec2(sin(ap), cos(ap));
-
-    float color = 5.0 - (6.0 * lp);
-
-    vec3 coord = vec3(atan(p.x, p.y) / 6.2832 + 0.5, 0.4 * lp, 0.5);
-
-    float power = 2.0;
-    for (int i = 0; i < 6; i++) {
-        power *= 2.0;
-        color += (1.5 / power) * snoise(coord + vec3(0.0, -0.05 * time*2.0, 0.01 * time*2.0), 16.0 * power);
-    }
-    color = max(color, 0.0);
-    float c2 = color * color;
-    float c3 = color * c2;
-    vec3 fc = vec3(color * 0.34, c2*0.15, c3*0.85);
-    float f = fract(time);
-    //fc *= smoothstep(f-0.1, f, length(p)) - smoothstep(f, f+0.1, length(p));
-    gl_FragColor = vec4(length(fc)*vec3(1,02,0)*0.04, 1.0);
+  gl_FragColor = vec4(o.rgb, 1.0);
 }
 `;
 
@@ -161,7 +124,7 @@ class StartScene extends Phaser.Scene {
   }
 
   create() {
-    const baseShader = new Phaser.Display.BaseShader('BufferShader1', fragmentShader);
+    const baseShader = new Phaser.Display.BaseShader('BufferShader1', fragmentShader1);
     const shader = this.add.shader(baseShader, 400, 300, 800, 600);
 
     const buttonGraphics = this.add.graphics();
@@ -195,12 +158,8 @@ class MainScene extends Phaser.Scene {
       .text(400, 32, "Click the bubbles to destroy them", { color: "#00ff00" })
       .setOrigin(0.5, 0);
     
-
-    this.graphics = this.add.graphics();
-    this.graphics.fillGradientStyle(0x0000ff, 0x0000ff, 0xffff00, 0xffff00, 1);
-    this.graphics.fillRect(0, 0, 800, 600);
-    
-
+      const baseShader = new Phaser.Display.BaseShader('BufferShader2', fragmentShader2);
+      const shader = this.add.shader(baseShader, 400, 300, 800, 600);
 
     this.graphics = this.add.graphics({
       lineStyle: { width: 4, color: 0xffffff },
