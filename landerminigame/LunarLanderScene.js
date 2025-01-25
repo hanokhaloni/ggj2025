df5db941954f1070280bba1dd4a1beaa7a51132c
Hanokh Aloni <hanokh.aloni@gmail.com>
2025-01-25 17:11:36 +0200

    added shader and terrain

--- landerminigame/LunarLanderScene.js
+++ landerminigame/LunarLanderScene.js
@@ -1,62 +1,81 @@
+import fragmentShader1 from "./fragmentShader1.js";
+
 class LunarLanderScene extends Phaser.Scene {
   constructor() {
     super({ key: "LunarLanderScene" });
-
+    this.hasCrashed = false;
   }
 
   preload() {
-    this.load.image('lander', 'bubble.png');
-    this.load.atlas('flares', 'flares.png', 'flares.json');
+    this.load.image("lander", "bubble.png");
+    this.load.atlas("flares", "flares.png", "flares.json");
+
   }
 
   create() {
 
-    this.emitter = this.add.particles(400, 250, 'flares', {
-      frame: [ 'red', 'yellow', 'green' ],
+    const baseShader = new Phaser.Display.BaseShader(
+      "BufferShader1",
+      fragmentShader1
+    );
+    const shader = this.add.shader(baseShader, window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight);
+
+
+    this.emitter = this.add.particles(400, 250, "flares", {
+      frame: ["red", "yellow", "green"],
       lifespan: 4000,
       speed: { min: 150, max: 250 },
       scale: { start: 0.8, end: 0 },
       gravityY: 150,
-      blendMode: 'ADD',
-      emitting: false
+      blendMode: "ADD",
+      emitting: false,
     });
 
-    this.lander = this.physics.add.sprite(400, 100, 'lander').setScale(0.05);
-    this.lander.setAngle(-90);
+    this.lander = this.physics.add.sprite(400, 100, "lander").setScale(0.05);
+    this.lander.setAngle(-100);
     this.lander.setCollideWorldBounds(true);
     this.lander.setDrag(100);
     this.lander.setAngularDrag(100);
     this.lander.setMaxVelocity(200);
     this.lander.setGravityY(300); // Add gravity force to the lander
 
-
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.ground = this.add.graphics();
-    this.ground.lineStyle(2, 0x00ff00, 1);
-    this.ground.beginPath();
-    this.ground.moveTo(0, 580);
-    this.ground.lineTo(200, 580);
-    this.ground.lineTo(300, 550);
-    this.ground.lineTo(500, 550);
-    this.ground.lineTo(600, 580);
-    this.ground.lineTo(800, 580);
-    this.ground.strokePath();
+    this.ground = this.physics.add.staticGroup();
+    let height = 700;
+    for (let i = 0; i < 50; i++) {
+      let box = this.add.rectangle(i * 16, height, 16, 20, 0x00ff00);
+      height += Phaser.Math.Between(-10, 10);
+      this.physics.add.existing(box, true);
+      box.rotation = Phaser.Math.Between(-45, 45);
+      this.ground.add(box);
+    }
 
-    this.physics.add.collider(this.lander, this.ground, this.checkLanding, null, this);
+    this.physics.add.collider(
+      this.lander,
+      this.ground,
+      this.checkLanding,
+      null,
+      this
+    );
 
     //add text for speed of lander
-    this.speedText = this.add.text(10, 10, "", { fontSize: "32px", fill: "#00ff00" });
-    
+    this.speedText = this.add.text(10, 10, "", {
+      fontSize: "32px",
+      fill: "#00ff00",
+    });
   }
 
   update() {
     this.emitter.setPosition(this.lander.x, this.lander.y);
     if (this.cursors.up.isDown) {
-      this.physics.velocityFromRotation(this.lander.rotation, 1000, this.lander.body.acceleration);
+      this.physics.velocityFromRotation(
+        this.lander.rotation,
+        1000,
+        this.lander.body.acceleration
+      );
 
       this.emitter.explode(3);
-      
     } else {
       this.lander.setAcceleration(0);
     }
@@ -69,25 +88,37 @@ class LunarLanderScene extends Phaser.Scene {
       this.lander.setAngularVelocity(0);
     }
 
-    let lunarSpeedString = "Speed: " +  this.lander.body.velocity.y.toFixed(2) + " m/s";
+    let lunarSpeedString =
+      "Speed: " + this.lander.body.velocity.y.toFixed(2) + " m/s";
     this.speedText.setText(lunarSpeedString);
-    // if (this.cursors.up.isDown) {
-    //   console.log(lunarSpeedString);
-    // }
+
   }
 
   checkLanding(lander, ground) {
-    console.log("CheckLanding...");
-    if (lander.body.velocity.y > 50) {
-      this.add.text(200, 300, "You crashed!!", { fontSize: "32px", fill: "#ff0000" }).setOrigin(0.5);
+    console.log(
+      "CheckLanding..." + lander.body.velocity.y + "  " + lander.body.velocity.x
+    );
+    if (
+      lander.body.velocity.y > 10.0 ||
+      lander.body.velocity.x > 10.0 ||
+      lander.body.velocity.x < -10.0
+    ) {
+      this.add
+        .text(200, 300, "You crashed!!", { fontSize: "32px", fill: "#ff0000" })
+        .setOrigin(0.5);
+      this.hasCrashed = true;
     } else {
       // Check if landed on flat surface
-      if (lander.body.touching.down && (lander.x > 200 && lander.x < 300 || lander.x > 500 && lander.x < 600)) {
-        this.add.text(200, 300, "You Landed!", { fontSize: "32px", fill: "#00ff00" }).setOrigin(0.5);
-      } else {
-        //this.scene.restart();
-        this.add.text(200, 300, "You crashed!", { fontSize: "32px", fill: "#ff0000" }).setOrigin(0.5);
+      //  if (lander.body.touching.down && (lander.x > 200 && lander.x < 300 || lander.x > 500 && lander.x < 600)) {
+      if (this.hasCrashed != true) {
+        this.add
+          .text(200, 300, "You Landed!", { fontSize: "32px", fill: "#00ff00" })
+          .setOrigin(0.5);
       }
+      //} else {
+      //this.scene.restart();
+      //this.add.text(200, 300, "You crashed!", { fontSize: "32px", fill: "#ff0000" }).setOrigin(0.5);
+      //}
     }
   }
 }
--- landerminigame/fragmentShader1.js
+++ landerminigame/fragmentShader1.js
@@ -1,83 +1,205 @@
 
 const fragmentShader1 = `
+/*
+ * Original shader from: https://www.shadertoy.com/view/tlfGRN
+ */
+
 #ifdef GL_ES
-precision mediump float;
+precision highp float;
 #endif
 
-uniform vec2 resolution;
-uniform vec2 mouse;
+// glslsandbox uniforms
 uniform float time;
+uniform vec2 resolution;
 
-float random (in vec2 _st) {
-    return fract(sin(dot(_st.xy,
-                         vec2(12.9898,78.233)))* 
-        43758.5453123);
+varying vec2 fragCoord;
+
+// shadertoy globals
+#define iTime time
+#define iResolution resolution
+const vec4  iMouse = vec4(0.0);
+
+// --------[ Original ShaderToy begins here ]---------- //
+#define EPS 0.0001
+#define PI 3.14159265359
+#define FLT_MAX 3.402823466e+38
+#define FLT_MIN 1.175494351e-38
+#define DBL_MAX 1.7976931348623158e+308
+#define DBL_MIN 2.2250738585072014e-308
+
+const int maxIterations = 64;
+const float stepScale = .9;
+const float stopThreshold = .005;
+
+float fov = .65;
+float nearClip = 0.;
+float farClip = 80.;
+
+struct Surface {
+    float dist;
+    vec3 position;
+    vec3 baseColor;
+    vec3 normal;
+    vec3 emissiveColor;
+};
+
+struct Hit {
+    Surface surface;
+    Surface near;
+    vec3 color;
+};
+
+float saturate(float s) {
+    return clamp(s, 0., 1.);
 }
 
-// Based on Morgan McGuire @morgan3d
-// https://www.shadertoy.com/view/4dS3Wd
-float noise (in vec2 _st) {
-    vec2 i = floor(_st);
-    vec2 f = fract(_st);
-
-    // Four corners in 2D of a tile
-    float a = random(i);
-    float b = random(i + vec2(1.0, 0.0));
-    float c = random(i + vec2(0.0, 1.0));
-    float d = random(i + vec2(1.0, 1.0));
-
-    vec2 u = f * f * (3.0 - 2.0 * f);
-
-    return mix(a, b, u.x) +
-            (c - a)* u.y * (1.0 - u.x) +
-            (d - b) * u.x * u.y;
+float smin(float a, float b, float k) {
+    float res = exp(-k * a) + exp(-k * b);
+    return -log(res) / k;
 }
 
-#define NUM_OCTAVES 5
+mat2 rot2(float t) {
+    return mat2(cos(t), -sin(t), sin(t), cos(t));
+}
 
-float fbm ( in vec2 _st) {
-    float v = 0.0;
-    float a = 0.5;
-    vec2 shift = vec2(100.0);
-    // Rotate to reduce axial bias
-    mat2 rot = mat2(cos(0.5), sin(0.5),
-                    -sin(0.5), cos(0.50));
-    for (int i = 0; i < NUM_OCTAVES; ++i) {
-        v += a * noise(_st);
-        _st = rot * _st * 2.0 + shift;
-        a *= 0.5;
+float scene(vec3 p) {
+    vec3 p1 = p;
+    p1.xy += vec2(iTime * .8 + 10., iTime * .4 + 20.);
+    p1.xy *= rot2(PI * .05);
+
+    vec3 p2 = p;
+    p2.yz += vec2(iTime * .4 + 30., iTime * .8 + 40.);
+    p2.yz *= rot2(PI * .04);
+
+    vec3 p3 = p;
+    p3.xz += vec2(iTime * .8 + 50., iTime * .6 + 60.);
+    p3.xz *= rot2(PI / 2. + iTime * .0);
+
+    float m = 6.;
+
+    p1.y += sin(sin(p1.z * 1.2 + iTime * 4.) * .3) * .3;
+    p1.x += sin(sin(p1.z * 1. + iTime * 2.) * .4) * .2;
+    p1.y = mod(p1.y, m) - m * .5;
+    p1.x = mod(p1.x, m) - m * .5;
+
+
+    p2.y += sin(sin(p2.z * 1.2 + iTime * 4.) * .4) * .4;
+    p2.x += sin(sin(p2.z * .5 + iTime * 3.) * .5) * .3;
+    p2.y = mod(p2.y, m) - m * .5;
+    p2.x = mod(p2.x, m) - m * .5;
+
+    p3.y += sin(sin(p3.z * .8 + iTime * 2.) * .4) * .2;
+    p3.x += sin(sin(p3.z * 1.1 + iTime * 3.) * .5) * .4;
+    p3.y = mod(p3.y, m) - m * .5;
+    p3.x = mod(p3.x, m) - m * .5;
+
+    float c = smin(length(p1.xy), length(p2.xy), 4.);
+    c = smin(c, length(p3.xy), 4.);
+
+    return c;
+}
+
+Hit rayMarching(vec3 origin, vec3 dir, float start, float end) {
+    Surface cs;
+    cs.dist = -1.;
+
+    Hit hit;
+    hit.color = vec3(0.);
+
+    float sceneDist = 0.;
+    float rayDepth = start;
+
+    for(int i = 0; i < maxIterations; i++) {
+        sceneDist = scene(origin + dir * rayDepth);
+
+        if((sceneDist < stopThreshold) || (rayDepth >= end)) {
+            break;
+        }
+        rayDepth += sceneDist * stepScale;
+        vec3 p = origin + dir * rayDepth;
+        vec3 c = sin((iTime + PI / 2.) * 4. * vec3(.123, .456, .789)) * .4 + .6;
+        hit.color += max(vec3(0.), .09 / sceneDist * c);
     }
-    return v;
+
+    /*
+    if (sceneDist >= stopThreshold) {
+        rayDepth = end;
+    } else {
+        rayDepth += sceneDist;
+    }
+    */
+
+    cs.dist = rayDepth;
+    hit.surface = cs;
+
+    return hit;
 }
 
-void main() {
-    vec2 st = gl_FragCoord.xy/resolution.xy*3.;
-    // st += st * abs(sin(time*0.1)*3.0);
-    vec3 color = vec3(0.0);
+vec3 fog(vec3 color, float distance, vec3 fogColor, float b) {
+    float fogAmount = 1. - exp(-distance * b);
+    return mix(color, fogColor, fogAmount);
+}
 
-    vec2 q = vec2(0.);
-    q.x = fbm( st + 0.00*time);
-    q.y = fbm( st + vec2(1.0));
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec2 mouse = iMouse.xy;
 
-    vec2 r = vec2(0.);
-    r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*time );
-    r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*time);
+    vec2 aspect = vec2(iResolution.x / iResolution.y, 1.);
+    vec2 screenCoord = (2. * fragCoord.xy / iResolution.xy - 1.) * aspect;
 
-    float f = fbm(st+r);
+    // displacement
+    vec2 uv = screenCoord;
+    uv.xy *= rot2(iTime * .07);
+    uv.y += sin(screenCoord.x * 2.4 + iTime * .05) * .16;
+    uv.x += sin(uv.y * 2.4 + iTime * .1) * .12;
 
-    color = mix(vec3(0.333333,0.333333,0.444444),
-                 vec3(0.33333,0.33333,0.498039),
-                 clamp((f*f)*4.0,0.0,1.0));
+    // mouse = mouse.xy / iResolution.xy - .5;
 
-    // color = mix(color,
-    //             vec3(0,0,0.164706),
-    //             clamp(length(q),0.0,1.0));
+    // camera settings
+    //vec3 lookAt = vec3(cos(iTime * .4) * .5, sin(iTime * .3) * .5, 0.);
+    float z = iTime * -5.;
+    vec3 lookAt = vec3(0., 0., z - 1.);
+    vec3 cameraPos = vec3(0., 0., z);
 
-     color = mix(color,
-                 vec3(1.0,1.0,1.0),
-                 clamp(length(r.x),0.0,1.0));
+    // camera vectors
+    vec3 forward = normalize(lookAt - cameraPos);
+    vec3 right = normalize(cross(forward, vec3(0., 1., 0.)));
+    vec3 up = normalize(cross(right, forward));
 
-    gl_FragColor = vec4((f*f*f+.6*f*f+.5*f)*color,1.0);
+    // raymarch
+    vec3 rayOrigin = cameraPos;
+    vec3 rayDirection = normalize(forward + fov * uv.x * right + fov * uv.y * up);
+    Hit hit = rayMarching(rayOrigin, rayDirection, nearClip, farClip);
+    Surface surface = hit.surface;
+
+    surface.position = rayOrigin + rayDirection * surface.dist;
+
+    // color
+    vec3 sceneColor = vec3(0.);
+
+    sceneColor = hit.color;
+
+    sceneColor = fog(sceneColor, surface.dist, vec3(0.), .065);
+
+    // vignet by channel
+    float vignetR = 1. - smoothstep(0., 2.5 + sin(iTime * 1.) * 1.5, length(screenCoord)) * .8;
+    float vignetG = 1. - smoothstep(0., 2.5 + cos(iTime * 1.2) * 1.5, length(screenCoord)) * .8;
+    float vignetB = 1. - smoothstep(0., 2.5 + sin(iTime * 1.4) * 1.5, length(screenCoord)) * .8;
+
+    sceneColor.x *= vignetR;
+    sceneColor.y *= vignetG;
+    sceneColor.z *= vignetB;
+
+    // debug distance color
+    //sceneColor.rgb = vec3(surface.dist / farClip);
+
+    fragColor = vec4(sceneColor, 1.);
+}
+
+// --------[ Original ShaderToy ends here ]---------- //
+
+void main(void)
+{
+    mainImage(gl_FragColor, fragCoord.xy);
 }
 `;
 
